83da8c01991111405a3835945b843b5db88196cc
Matt Faircloth <mattcfaircloth@gmail.com>
2017-11-16 18:16:56 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,6 +1,6 @@
 def alphabetize(arr)
-  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  
+  esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+
   arr.sort_by { |phrase| phrase.split("").map{ |char| ESPERANTO_ALPHABET.index(char) } }
-  
+
 end
